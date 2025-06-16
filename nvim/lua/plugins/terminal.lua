ed29b65d6fe1e72e4f1866e949890a3976d74345
eternalcake <eternalcake.sake@gmail.com>
2025-06-16 15:48:14 +0300

    update lazygit term

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,40 +1,45 @@
 return {
-  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function()
-      -- Set up terminal based on OS
-      local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
-      if is_windows then
-        vim.o.shell = "pwsh.exe"
-        vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
-        vim.o.shellquote = ""
-        vim.o.shellxquote = ""
-        -- else
-        --   vim.o.shell = "/bin/zsh"
-      end
-      -- Keymap
-      require("toggleterm").setup {
-        open_mapping = [[<c-\>]],
-      }
-      -- LazyGit terminal settings
-      local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit  = Terminal:new({
-        cmd = "lazygit",
-        hidden = true,
-        display_name = "LazyGit",
-        direction = "float",
-        float_opts = {
-          border = "curved",
-          width = 120,
-          height = 30,
-        },
-      })
-      function _lazygit_toggle()
-        lazygit:toggle()
-      end
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			-- Set up terminal based on OS
+			local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+			if is_windows then
+				vim.o.shell = "pwsh.exe"
+				vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+				vim.o.shellquote = ""
+				vim.o.shellxquote = ""
+				-- else
+				--   vim.o.shell = "/bin/zsh"
+			end
+			-- Keymap
+			require("toggleterm").setup({
+				open_mapping = [[<c-\>]],
+			})
+			-- LazyGit terminal settings
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({
+				cmd = "lazygit",
+				hidden = true,
+				display_name = "LazyGit",
+				direction = "float",
+				float_opts = {
+					border = "curved",
+					width = 150,
+					height = 40,
+				},
+			})
+			function _lazygit_toggle()
+				lazygit:toggle()
+			end
 
-      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
-    end,
-  }
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>g",
+				"<cmd>lua _lazygit_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 }
