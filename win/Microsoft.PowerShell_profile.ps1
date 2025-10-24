dac463f5996b7920df9dbc7e715d524767d28da4
eternalcake <eternalcake.sake@gmail.com>
2025-10-24 12:49:33 +0300

    update

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,4 @@
-# oh-my-posh init pwsh --config pure| Invoke-Expression
-#Import-Module pure-pwsh
-#Import-Module posh-git
+Import-Module pure-pwsh
+Import-Module posh-git
+
 Set-Alias -Name vim -Value nvim
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-# if (Test-Path($ChocolateyProfile)) {
-#   Import-Module "$ChocolateyProfile"
-# }
-
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-#Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
