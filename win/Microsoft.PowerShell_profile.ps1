ca76eed942894a14e80713d607d07c55783cdd11
eternalcake <eternalcake.sake@gmail.com>
2025-10-25 14:35:40 +0300

    update

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,11 @@
 Import-Module pure-pwsh
 Import-Module posh-git
 
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\eternalcake\.config\komorebi'
+
 Set-Alias -Name vim -Value nvim
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
